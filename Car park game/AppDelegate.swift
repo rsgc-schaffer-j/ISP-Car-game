318cb77c7ce17a656d596fedff90a4ee2f07428c
Jasper <Jasper@macbook-pro-0663-jasper-schaffer.local>
2017-02-28 13:41:34 -0500

    Initial Commit

---
+++ Car park game/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Car park game
+//
+//  Created by Jasper on 2017-02-28.
+//  Copyright © 2017 Jasper. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
