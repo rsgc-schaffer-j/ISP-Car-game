c8e1c6b8775a04e648778b047b55c5519620fdd5
Jasper <Jasper@macbook-pro-0663-jasper-schaffer.local>
2017-02-28 14:28:24 -0500

    I have my scene set up. basic white background

--- Car park game/ViewController.swift
+++ Car park game/ViewController.swift
@@ -11,27 +11,22 @@ import SpriteKit
 import GameplayKit
 
 class ViewController: NSViewController {
-
+    
     @IBOutlet var skView: SKView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        if let view = self.skView {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
-        }
+        let scene = GameScene(size: CGSize(width: 2048, height:1536))
+        
+        let skView = self.view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        
+        skView.ignoresSiblingOrder = true
+        scene.scaleMode = .aspectFill
+        skView.presentScene(scene)
     }
+    
+
 }
 
