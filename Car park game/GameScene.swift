56c2f20526b2dc21b49b78f6e67469b25492ff60
Jasper <Jasper@macbook-pro-0663-jasper-schaffer.local>
2017-03-06 13:30:49 -0500

    car moment added

--- Car park game/GameScene.swift
+++ Car park game/GameScene.swift
@@ -11,8 +11,42 @@ import GameplayKit
 
 class GameScene: SKScene {
     
+    let midPoint = CGPoint()
+    var car = SKSpriteNode()
+    
     override func didMove(to view : SKView){
         
         backgroundColor = SKColor.white
+        
+        addObjects()
     }
+    
+    func addObjects() {
+        
+        car = SKSpriteNode(imageNamed: "Car")
+        car.name = "Car"
+        car.position = CGPoint(x: 200, y: 250)
+        car.setScale(0.6)
+        self.addChild(car)
+    }
+    
+    override func keyDown(with event: NSEvent) {
+        //left =123
+        //right=124
+        //down=125
+        //up=126
+        while event.keyCode == 126 {
+            let move = SKAction.moveBy(x: 0,y: 200,duration:2)
+            car.run(move)
+            
+        }
+        
+        while event.keyCode == 125 {
+            let move = SKAction.moveBy(x: 0,y: -200,duration:2)
+            car.run(move)
+            
+        }
+        
+    }
+    
 }
