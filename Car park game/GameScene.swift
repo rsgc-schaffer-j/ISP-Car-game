80e68d66264ebe6c8a5dd86086fb39df318645ab
Jasper <Jasper@macbook-pro-0663-jasper-schaffer.local>
2017-03-29 09:21:38 -0400

    the sprite has started to rotate with the car

--- Car park game/GameScene.swift
+++ Car park game/GameScene.swift
@@ -17,7 +17,9 @@ class GameScene: SKScene {
     let midPoint = CGPoint()
     var car = SKSpriteNode()
     var rotation: CGFloat = CGFloat(M_PI)/2
-    
+    var currentRotate = CGFloat(M_PI)/2
+    var neededRoate = CGFloat(0.000)
+    var drive: Bool = true
     override func didMove(to view : SKView){
         
         backgroundColor = SKColor.white
@@ -27,6 +29,7 @@ class GameScene: SKScene {
     
     func addObjects() {
         
+        
         car = SKSpriteNode(imageNamed: "Car")
         car.name = "Car"
         car.position = CGPoint(x: 200, y: 250)
@@ -35,24 +38,34 @@ class GameScene: SKScene {
     }
     
     override func update(_ currentTime: TimeInterval) {
+       
+        if fast > 0{
+            drive = true
+        }else{
+            drive = false
+        }
+        
         fast = xMovement + yMovement
         if fast > 10 || fast < -10 {
             acceloration = 1
         }
         
-        if acceloration == 2 || acceloration == 0.5{
+    //    if acceloration == 2 || acceloration == 0.5{
             yMovement = yMovement * acceloration
             xMovement = xMovement * acceloration
-        }else if fast < -0.3 || fast > 0.3{
-           // yMovement = yMovement * 0.9
-          //  xMovement = xMovement * 0.9
-        }else{
-            xMovement = 0
-            yMovement = 0
-        }
-        
+//        }else if fast < -0.3 || fast > 0.3{
+//           yMovement = yMovement * 0.9
+//        xMovement = xMovement * 0.9
+//        }else{
+//            xMovement = 0
+//            yMovement = 0
+//        }
+        neededRoate = currentRotate - rotation * -1 
+        currentRotate = rotation * -1
+        let action = SKAction.rotate(byAngle: neededRoate, duration: 0.2)
         let move = SKAction.moveBy(x: xMovement,y:yMovement,duration:0.001)
         
+        car.run(action)
         car.run(move)
         print(fast)
     }
@@ -64,44 +77,82 @@ class GameScene: SKScene {
         //down=125
         //up=126
         
-        xMovement = cos(rotation)
-        yMovement = sin(rotation)
-        
         fast = xMovement + yMovement
         if event.keyCode == 126 {
-            if fast>0.3 && fast<60 {
-                acceloration = 2
-            }else if fast < 0.3 && fast > -0.3 && fast != 0{
-                fast=0
-            }else if fast < -0.4 {
-                acceloration = 0.5
-            }else{
-                acceloration = 2
-            }
-        }
+           // if drive {
+                xMovement = cos(rotation)*2
+                yMovement = sin(rotation)*2
+             acceloration = 1
+          //  }
+                    }
         
         if event.keyCode == 125{
-            if fast>0.3 && fast<60 {
-                acceloration = 0.5
-            }else if fast < 0.3 && fast > -0.3 && fast != 0{
-                fast=0
-            }else if fast < -0.3 && fast > -60 {
-                acceloration = 2
-            }else{
-                xMovement = xMovement * -1
-                yMovement = yMovement * -1
-                acceloration = 2
-            }
-        }
+           //if !drive {
+                xMovement = cos(rotation) * -2
+                yMovement = sin(rotation) * -2
+                acceloration = 1
+            //}
+                   }
+        
         if event.keyCode == 124{
-                 rotation -= 0.0872665
+                 rotation -= 0.523599
+            if fast > 0{
+                xMovement = cos(rotation)*2
+                yMovement = sin(rotation)*2
+            }else if fast < 0{
+                xMovement = cos(rotation) * -2
+                yMovement = sin(rotation) * -2
+            }
         }
         
         if event.keyCode == 123{
-            rotation += 0.0872665
+            rotation += 0.523599
+            if fast > 0 && rotation < CGFloat(M_PI) || fast < 0 && rotation > CGFloat(M_PI) {
+                xMovement = cos(rotation)*2
+                yMovement = sin(rotation)*2
+            }else if fast < 0 && rotation < CGFloat(M_PI) || fast > 0 && rotation > CGFloat(M_PI){
+                xMovement = cos(rotation) * -2
+                yMovement = sin(rotation) * -2
+            }
+
         }
-        
+     //   print(event.keyCode)
         
     }
     
+    
+    
 }
+
+
+
+//if event.keyCode == 126 {
+//    if fast>0.3 && fast<60 {
+//        acceloration = 2
+//    }else if fast < 0.3 && fast > -0.3 && fast != 0{
+//        fast=0
+//    }else if fast < -0.4 {
+//        acceloration = 0.5
+//    }else{
+//        acceloration = 2
+//    }
+//    xMovement = cos(rotation)*2
+//    yMovement = sin(rotation)*2
+//}
+//
+//if event.keyCode == 125{
+//    if fast>0.3 && fast<60 {
+//        acceloration = 0.5
+//    }else if fast < 0.3 && fast > -0.3 && fast != 0{
+//        fast=0
+//    }else if fast < -0.3 && fast > -60 {
+//        acceloration = 2
+//    }else{
+//        xMovement = xMovement * -1
+//        yMovement = yMovement * -1
+//        acceloration = 2
+//    }
+//    xMovement = cos(rotation)*2
+//    yMovement = sin(rotation)*2
+//}
+
